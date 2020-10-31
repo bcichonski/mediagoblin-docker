ca041c169acb362b5b9b7a835c39e091165d9232
bartq <bci@poczta.onet.pl>
2020-10-31 19:52:46 +0100

    Try to fix connection issues

--- postgres/init-db.sh
+++ postgres/init-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE ROLE mediagoblin WITH NOCREATEDB LOGIN;
+    CREATE ROLE mediagoblin WITH NOCREATEDB LOGIN PASSWORD 'medi4goblinp455';
     CREATE DATABASE mediagoblin;
     GRANT ALL PRIVILEGES ON DATABASE mediagoblin TO mediagoblin;
 EOSQL
