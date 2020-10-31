9901f720cb447595a8c02d7a8916c37460bc55da
bartq <bci@poczta.onet.pl>
2020-10-31 17:42:53 +0100

    Upgrade pipeline

--- mediagoblin/init-mediagoblin.sh
+++ mediagoblin/init-mediagoblin.sh
@@ -11,6 +11,7 @@ bin/gmg dbupdate
     --password "$MEDIAGOBLIN_ADMIN_PASS" \
     --email "$MEDIAGOBLIN_ADMIN_EMAIL" && \
   bin/gmg makeadmin "$MEDIAGOBLIN_ADMIN_USER"
+  bin/gmg dbupdate
 } || true
 
 # Create a default mediagoblin.ini if none has been specified.
--- postgres/init-db.sh
+++ postgres/init-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER mediagoblin;
+    CREATE ROLE mediagoblin WITH NOCREATEDB LOGIN;
     CREATE DATABASE mediagoblin;
     GRANT ALL PRIVILEGES ON DATABASE mediagoblin TO mediagoblin;
 EOSQL
